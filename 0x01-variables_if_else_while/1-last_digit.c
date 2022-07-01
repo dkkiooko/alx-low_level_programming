25eaa8596537db9d06228ede86393b2ce18ad480
Daniel Kioko <kithekadankioko@gmail.com>
2022-07-01 10:21:17 -0700

    use of logical operators in if statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,19 +1,27 @@
+/*
+ * File: 1-last_digit.c
+ * Auth: Daniel Kioko
+ */
 #include <stdlib.h>
 #include <time.h>
-
+#include <stdio.h>
+/**
+ * main - is zero, >5 or <5
+ * Return: Always 0.
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int l = n % 10;
-	
-	if (l==0)
-		printf("Last digit of %d is %d and is 0\n", n, l);
-	else if (l>5)
-		printf("Last digit of %d is %d and is greater than 5", n, l);
+	int d;
+
+	d = n % 10;
+	if (d > 5)
+		printf("Last digit of %d is %d and is greater than 5", n, d);
+	else if (d > 0 && d < 6)
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, d);
 	else
-		printf("Last digit of %d is %d and is less than 6 and not 0", n, l);
-	return (0);
+		printf("Last digit of %d is %d and is 0", n, d);
 }
