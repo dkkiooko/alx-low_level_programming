4eed11eb6a1457811f8fe7bb0df9f42114bf587b
Daniel Kioko <kithekadankioko@gmail.com>
2022-07-01 10:28:57 -0700

    use of logical operators in if statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,8 +21,7 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n, d);
 	else if (d > 0 && d < 6)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, d);
-	else
+	else if (d < 1)
 		printf("Last digit of %d is %d and is 0\n", n, d);
-	
 	return (0);
 }
