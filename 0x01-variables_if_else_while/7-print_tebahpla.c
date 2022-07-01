c16bf551b3a50c6c6e76ead10cdc165702d6f937
Daniel Kioko <kithekadankioko@gmail.com>
2022-07-01 11:28:07 -0700

    Using a for loop in a decremental manner

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+/*
+ * File - 7-print_tebahpla.c
+ * Auth: Daniel Kioko
+ */
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints alphabet in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'z'; x <= 'a'; x--)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
