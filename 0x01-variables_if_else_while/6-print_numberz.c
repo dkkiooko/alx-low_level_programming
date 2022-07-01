8780e3550c0914ca3756a4eaf5636904d3c1316c
Daniel Kioko <kithekadankioko@gmail.com>
2022-07-01 11:22:45 -0700

    print numbers using putchar() function

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+/*
+ * File - 6-print_numberz.c
+ * Auth: Daniel Kioko
+ */
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print 0 - 9 with putchar
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+	purchar('\n');
+	return (0);
+}
