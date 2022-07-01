430ae2e747af2825c9713292a0a3dc09e90c6ce1
Daniel Kioko <kithekadankioko@gmail.com>
2022-07-01 11:13:56 -0700

    Use of printf with a for loop

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+/*
+ * File - 5-print_numbers.c
+ * Auth: Daniel Kioko
+ */
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints numbers 0 to 9
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 10; x++)
+		printf("%d", x);
+	return (0);
+}
