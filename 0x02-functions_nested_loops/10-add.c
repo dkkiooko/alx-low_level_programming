bf5e9097032fb899a46849268d953d871a4b73e8
Daniel Kioko <kithekadankioko@gmail.com>
2022-07-05 14:34:28 -0700

    add 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+/*
+ * File - 10-add.c
+ * Auth: Daniel K Kioko
+ */
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - adds 2 integers
+ * @i: the first number to be added
+ * @j: the second number to be added
+ * Return: the sum of two numbets
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
