8b9bc4330b44cd234480af8c05170dc634880abe
Daniel Kioko <kithekadankioko@gmail.com>
2022-07-05 23:45:12 -0700

    use of logical or an conditions

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+/*
+ * File - 101-natural.c
+ * Auth: Daniel K Kioko
+ */
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints sum of multiples of
+ * 3 and 5 under `1024
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, res;
+
+	for (i = 0; i < 1024; i++)
+		if (i % 3 == 0 || i % 5 == 0)
+			res += i;
+		printf("%d\n", res);
+		return (0);
+}
