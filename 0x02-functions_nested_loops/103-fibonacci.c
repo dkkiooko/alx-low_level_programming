3991a1eae15f7ec10ca8a756bf7d957f1af37394
Daniel Kioko <kithekadankioko@gmail.com>
2022-07-06 00:10:57 -0700

    another fibonacci sequence with conditions evennes and under 4 million

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+/*
+ * File - 103-fibonacci.c
+ * Auth: Daniel K Kioko
+ */
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - sum even fibonacci numbers under 4 million
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long count, i, j, k, sums;
+
+	i = sums = 0;
+	j = 1;
+	for (count = 0; count < 50; count++)
+	{
+		k = i + j;
+		i = j;
+		j = k;
+		if (k % 2 == 0 && k < 4000000)
+		{
+			sums += k;
+		}
+	}
+	printf("%lu\n", sums);
+	return (0);
+}
