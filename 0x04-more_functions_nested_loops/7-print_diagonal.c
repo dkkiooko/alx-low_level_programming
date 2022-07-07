6adebccc74b489d255cdf915d18807d23797103c
Daniel Kioko <kithekadankioko@gmail.com>
2022-07-07 13:39:17 -0700

    nested loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+/*
+ * File - 7-print_diagonal.c
+ * Auth: Daniel K Kioko
+ */
+#include "main.h"
+/**
+ * print_diagonal - prints a diagonal of size n
+ * @n: length of diagonal
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		int a, b;
+
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b <= a; b++)
+			{
+				if (a != b)
+					_putchar(' ');
+				else
+					_putchar('\n');
+			}
+		}
+	}
+	else
+		_putchar('\n')
+}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+/*
+ * Auth: Dan
+ */
+#include "main.h"
+/**
+ * print_square - prints square made of #
+ * @size: length of side of square
+ * Return: nothing
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 1; b < size; b++)
+			{
+				_putchar('#');
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
