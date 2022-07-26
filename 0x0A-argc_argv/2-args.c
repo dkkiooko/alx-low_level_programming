4be4378ff220e8993b229764ac81f6c15935848b
Daniel Kioko <kithekadankioko@gmail.com>
2022-07-26 00:48:02 -0700

    a missed function

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the number of arguments
+ * @argc: argument count
+ * @argv: argument vector for values
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
