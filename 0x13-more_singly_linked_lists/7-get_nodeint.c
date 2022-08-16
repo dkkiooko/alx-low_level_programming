8d3dccf5bfcbd44a72204668dfe3d9bbf12cd1fe
Daniel Kioko <kithekadankioko@gmail.com>
2022-08-16 09:06:27 -0700

    correction

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,4 +9,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int n = 0;
 
-	while (head
+	while (head != NULL)
+	{
+		if (n == index)
+			return (head);
+		head = head->next;
+		n++;
+	}
+	return (NULL);
+}
