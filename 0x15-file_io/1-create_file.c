cb646d8c9fb860a9b9d5c0645eb699dad344faa3
Daniel Kioko <kithekadankioko@gmail.com>
2022-08-22 06:53:44 -0700

    betty error corrections

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * append_text_to_file - appends text at the end of a file
+ * create_file - appends text at the end of a file
  * @filename: name of the file
  * @text_content: NULL terminated string to add
  * Return: 1 on success and -1 on failure
@@ -12,18 +12,22 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
+	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (file == -1)
 		return (-1);
 
 	while (text_content[i] != '\0')
 		i++;
 
-	file = open(filename, 0_CREAT | 0_WRONLY | O_TRUNC, 0600);
-
-	if (file == -1)
+	if (text_content == NULL)
+	{
+		close(file);
 		return (-1);
-
-	write(file, text_content, i);
+	}
+	else
+	{
+		write(file, text_content, i);
+	}
 	close(file);
 
 	return (1);
