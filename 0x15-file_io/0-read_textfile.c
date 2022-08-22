f3ced5d346f9b2f3b4f8d5ebc79e10ca7cae36c2
Daniel Kioko <kithekadankioko@gmail.com>
2022-08-22 06:30:45 -0700

    correct typo

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,30 +7,32 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file, rd, wr;
+	ssize_t file, rd, wr;
 	char *text;
 
 	if (filename == NULL)
-		return (0);
-
-	file = open(filename, O_RDONLY);
-	if (file == -1)
 		return (0);
 
 	text = malloc(letters);
 	if (text == NULL)
 		return (0);
 
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+	{
+		free(text);
+		return (0);
+	}
+
 	rd = read(file, text, letters);
 	if (rd == -1)
 		return (0);
 
-	text[letters] = '\0';
 	wr = write(STDOUT_FILENO, file, rd);
 	if (wr == -1)
 		return (0);
 
 	close(file);
-	free(buf);
+	free(text);
 	return (wr);
 }
