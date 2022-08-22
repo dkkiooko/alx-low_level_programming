1f75400b8f04ae8b12eb05d6e796d999e567c142
Daniel Kioko <kithekadankioko@gmail.com>
2022-08-22 06:34:04 -0700

    correct typo

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -28,7 +28,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (rd == -1)
 		return (0);
 
-	wr = write(STDOUT_FILENO, file, rd);
+	wr = write(STDOUT_FILENO, text, rd);
 	if (wr == -1)
 		return (0);
 
