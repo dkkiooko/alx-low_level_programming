281db87c98eb36ed941e4459b96819f59ff88299
dkkiooko <kithekadankioko@gmail.com>
2022-12-14 09:54:57 +0300

    python errors

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 """module that finds perimeter"""
 
+
 def island_perimeter(grid):
+    """gets perimeter
+
+    Args:
+        grid (_list_): _list of 1s(land) and 0s(water)_
+
+    Returns:
+        _type_: _description_
+    """
     c = 0
     length = len(grid) - 1
     width = len(grid[0]) - 1
@@ -9,7 +18,7 @@ def island_perimeter(grid):
     for i, r in enumerate(grid):
         for j, n in enumerate(r):
             if n == 1:
-                if i == 0 or grid [i - 1][j] != 1:
+                if i == 0 or grid[i - 1][j] != 1:
                     c += 1
                 if j == 0 or grid[i][j - 1] != 1:
                     c += 1
